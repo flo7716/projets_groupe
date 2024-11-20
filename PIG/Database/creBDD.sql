ddb2d9ceffe9de9e9911e450f76dcd144803f117
flori <161379261+flo7716@users.noreply.github.com>
2024-11-20 20:54:11 +0100

    added enterprise on sql script

--- PIG/Database/creBDD.sql
+++ PIG/Database/creBDD.sql
@@ -3,16 +3,36 @@ CREATE TABLE ASSOCIATION(
     nomAsso VARCHAR(255) NOT NULL,
     domaineAsso VARCHAR(255) NOT NULL,
     PRIMARY KEY(idAsso)
-)
+);
 
 
 CREATE TABLE EVENEMENT_ORGANISE_PAR_ASSO(
     idAsso VARCHAR(255) NOT NULL,
     idEvent VARCHAR(255) NOT NULL,
     dateEvent DATE NOT NULL,
+    lieuEvent VARCHAR(255) NOT NULL,
     descriptionEvent VARCHAR(255) NOT NULL,
     PRIMARY KEY(idAsso, idEvent),
     FOREIGN KEY(idAsso) REFERENCES ASSOCIATION(idAsso)
+);
+
+
+CREATE TABLE ENTREPRISE(
+    idEntreprise VARCHAR(255) NOT NULL,
+    nomEntreprise VARCHAR(255) NOT NULL,
+    domaineEntreprise VARCHAR(255) NOT NULL,
+    PRIMARY KEY(idEntreprise)
+);
+
+
+CREATE TABLE EVENEMENT_ORGANISE_PAR_ENTREPRISE(
+    idEntreprise VARCHAR(255) NOT NULL,
+    idEvent VARCHAR(255) NOT NULL,
+    dateEvent DATE NOT NULL,
+    lieuEvent VARCHAR(255) NOT NULL,
+    descriptionEvent VARCHAR(255) NOT NULL,
+    PRIMARY KEY(idEntreprise, idEvent),
+    FOREIGN KEY(idEntreprise) REFERENCES ENTREPRISE(idEntreprise)
 );
 
 
