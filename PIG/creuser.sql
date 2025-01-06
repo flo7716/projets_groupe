9ce75127d28d9420488a45f9ba900112d412e002
flori <161379261+flo7716@users.noreply.github.com>
2025-01-06 11:53:43 +0100

    updated creuser

--- PIG/creuser.sql
+++ PIG/creuser.sql
@@ -1,12 +1,8 @@
--- Générer des mots de passe aléatoires
-SET @asso_password = SUBSTRING(MD5(RAND()), 1, 16);  -- Génère un mot de passe aléatoire de 16 caractères
-SET @news_password = SUBSTRING(MD5(RAND()), 1, 16);  -- Génère un mot de passe aléatoire de 16 caractères
+-- Créer l'utilisateur pour la base de données assos avec le mot de passe
+CREATE USER 'asso_scraper'@'%' IDENTIFIED BY '37867e0066c7214d';  -- Utilisez le mot de passe généré
 
--- Créer l'utilisateur pour la base de données assos
-CREATE USER 'asso_scraper'@'%' IDENTIFIED BY @asso_password;
-
--- Créer l'utilisateur pour la base de données news_db
-CREATE USER 'news_scraper'@'%' IDENTIFIED BY @news_password;
+-- Créer l'utilisateur pour la base de données news_db avec le mot de passe
+CREATE USER 'news_scraper'@'%' IDENTIFIED BY 'cc62b1fad6b1061f';  -- Utilisez le mot de passe généré
 
 -- Attribuer des droits uniquement à la base de données assos pour l'utilisateur asso_scraper
 GRANT SELECT, INSERT, UPDATE, DELETE ON assos.* TO 'asso_scraper'@'%';
@@ -16,6 +12,3 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON news_db.* TO 'news_scraper'@'%';
 
 -- Appliquer les changements de privilèges
 FLUSH PRIVILEGES;
-
--- Afficher les mots de passe générés pour chaque utilisateur
-SELECT @asso_password AS 'asso_scraper_password', @news_password AS 'news_scraper_password';
