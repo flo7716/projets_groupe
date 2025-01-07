05eb265b94e657197af4ee7a1a762d464e03bfe5
flori <161379261+flo7716@users.noreply.github.com>
2025-01-07 15:14:43 +0100

    modified credb.sql

--- credb.sql
+++ credb.sql
@@ -3,19 +3,36 @@ CREATE DATABASE IF NOT EXISTS assos;
 
 USE assos;
 
+-- Table des utilisateurs
+CREATE TABLE IF NOT EXISTS USER (
+    idUser INT AUTO_INCREMENT PRIMARY KEY,
+    nomUser VARCHAR(255) NOT NULL,
+    prenomUser VARCHAR(255) NOT NULL,
+    emailUser VARCHAR(255) NOT NULL UNIQUE,
+    passwordUser VARCHAR(255) NOT NULL,
+    roleUser ENUM('admin', 'president') NOT NULL
+);
+
+-- Table des associations
 CREATE TABLE IF NOT EXISTS ASSOCIATION (
     idAsso INT AUTO_INCREMENT PRIMARY KEY,
     nomAsso VARCHAR(255) NOT NULL UNIQUE,
-    domaineAsso VARCHAR(255) NOT NULL
+    domaineAsso VARCHAR(255) NOT NULL,
+    presidentId INT NOT NULL,
+    FOREIGN KEY (presidentId) REFERENCES USER(idUser) -- Associe un président
 );
 
+
+-- Table des événements
 CREATE TABLE IF NOT EXISTS EVENEMENT_ORGANISE_PAR_ASSO (
     idEvent INT AUTO_INCREMENT PRIMARY KEY,
     shortCode VARCHAR(255) NOT NULL UNIQUE,
-    nomAsso VARCHAR(255) NOT NULL,
+    idAsso INT NOT NULL,
     descriptionEvent TEXT,
-    FOREIGN KEY (nomAsso) REFERENCES ASSOCIATION(nomAsso)
+    dateEvent DATETIME NOT NULL,
+    FOREIGN KEY (idAsso) REFERENCES ASSOCIATION(idAsso)
 );
+
 
 -- Create the news_db database and its tables
 CREATE DATABASE IF NOT EXISTS news_db;
