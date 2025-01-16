e946afcffdb8a73cc3085b5be6805cad83383c51
flori <161379261+flo7716@users.noreply.github.com>
2025-01-16 10:21:30 +0100

    deleted port 80

--- Projet_federateur_IA/app.py
+++ Projet_federateur_IA/app.py
@@ -56,5 +56,5 @@ def add_article():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0")
 
