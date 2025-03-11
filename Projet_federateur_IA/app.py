10e5c340e6dca7b9643fb61d84019bd7e07524e6
flo7716 <florian.andre.77@gmail.com>
2025-03-11 19:00:11 +0100

    debug false

--- Projet_federateur_IA/app.py
+++ Projet_federateur_IA/app.py
@@ -74,4 +74,4 @@ def delete_article(article_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=False, port=5000, host='0.0.0.0')
