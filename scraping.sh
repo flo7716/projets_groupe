07714c01334e8c65deb39c12cc15dbe9000616b1
flori <161379261+flo7716@users.noreply.github.com>
2025-03-22 23:06:24 +0100

    Update scraping.sh for Jenkins execution

--- scraping.sh
+++ scraping.sh
@@ -2,13 +2,17 @@
 ENV_LOCATION=/home/ubuntu/env/bin
 SCRIPT_LOCATION=/home/ubuntu/projets_groupe/Projet_federateur_IA
 
-
-#Activation environnement Python
+# Activation de l'environnement Python
 source $ENV_LOCATION/activate
 
-#Execution scraping
-echo "Execution script Python scraping"
-python $SCRIPT_LOCATION/scraper.py
-echo "Script termine"
+# Exécution du script de scraping et redirection des erreurs vers un fichier log
+echo "Exécution du script Python de scraping"
+python $SCRIPT_LOCATION/scraper.py > /home/ubuntu/scraping.log 2>&1
 
+# Vérification du code de sortie de Python
+if [ $? -ne 0 ]; then
+    echo "Erreur lors de l'exécution du script Python de scraping. Voir le log pour plus de détails."
+    exit 1
+fi
 
+echo "Script terminé"
