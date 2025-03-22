4d196e44d17c4124c6f223ccce6d73a9692aa757
flo7716 <florian.andre.77@gmail.com>
2025-03-22 23:30:10 +0100

    retrait accents

--- scraping.sh
+++ scraping.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 set -x  # Enable debugging
-ENV_LOCATION=/home/ubuntu/env/bin
-SCRIPT_LOCATION=/home/ubuntu/projets_groupe/Projet_federateur_IA
 
-# Activation de l'environnement Python
+# Define environment and script locations
+ENV_LOCATION="/home/ubuntu/env/bin"
+SCRIPT_LOCATION="/home/ubuntu/projets_groupe/Projet_federateur_IA"
+
+# Activate the Python virtual environment
 if [ ! -f "$ENV_LOCATION/activate" ]; then
     echo "Erreur: Le fichier d'activation de l'environnement Python est introuvable."
     exit 1
 fi
-source $ENV_LOCATION/activate
+source "$ENV_LOCATION/activate"
 
-# Exécution du script de scraping et redirection des erreurs vers un fichier log
-echo "Exécution du script Python de scraping"
+# Execute the Python scraping script and redirect errors to a log file
+echo "Execution du script Python de scraping"
 if [ ! -f "$SCRIPT_LOCATION/scraper.py" ]; then
     echo "Erreur: Le script Python de scraping est introuvable."
     exit 1
 fi
-python $SCRIPT_LOCATION/scraper.py > /home/ubuntu/scraping.log 2>&1
 
-# Vérification du code de sortie de Python
+python "$SCRIPT_LOCATION/scraper.py" > /home/ubuntu/scraping.log 2>&1
+
+# Check the exit code of the Python script
 if [ $? -ne 0 ]; then
-    echo "Erreur lors de l'exécution du script Python de scraping. Voir le log pour plus de détails."
+    echo "Erreur lors de l'execution du script Python de scraping. Voir le log pour plus de details."
     exit 1
 fi
 
-echo "Script terminé"
+echo "Script termine"
