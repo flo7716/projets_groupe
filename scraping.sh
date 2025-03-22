aa71b8759ff43155ac362b3647537b963a019660
flo7716 <florian.andre.77@gmail.com>
2025-03-22 23:17:02 +0100

    ajout debug

--- scraping.sh
+++ scraping.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+set -x  # Enable debugging
 ENV_LOCATION=/home/ubuntu/env/bin
 SCRIPT_LOCATION=/home/ubuntu/projets_groupe/Projet_federateur_IA
 
 # Activation de l'environnement Python
+if [ ! -f "$ENV_LOCATION/activate" ]; then
+    echo "Erreur: Le fichier d'activation de l'environnement Python est introuvable."
+    exit 1
+fi
 source $ENV_LOCATION/activate
 
 # Exécution du script de scraping et redirection des erreurs vers un fichier log
 echo "Exécution du script Python de scraping"
+if [ ! -f "$SCRIPT_LOCATION/scraper.py" ]; then
+    echo "Erreur: Le script Python de scraping est introuvable."
+    exit 1
+fi
 python $SCRIPT_LOCATION/scraper.py > /home/ubuntu/scraping.log 2>&1
 
 # Vérification du code de sortie de Python
