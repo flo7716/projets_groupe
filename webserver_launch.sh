57bb2060809ea0dbeeb17c1793f76b13390f64ba
flo7716 <florian.andre.77@gmail.com>
2025-04-03 17:55:46 +0000

    modif chemin ec2

--- webserver_launch.sh
+++ webserver_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ENV_LOCATION=$HOME_DIR/env/bin
-SCRIPT_LOCATION=$HOME_DIR/Documents/projets_groupe/Projet_federateur_IA
+SCRIPT_LOCATION=$HOME_DIR/projets_groupe/Projet_federateur_IA
 
 #activation environnement virtuel
 source $ENV_LOCATION/activate
