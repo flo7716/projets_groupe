5eff69f4756a9512a2529a7784fdea44c21856c3
flo7716 <florian.andre.77@gmail.com>
2025-04-03 15:27:57 +0200

    pwd

--- webserver_launch.sh
+++ webserver_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ENV_LOCATION=/home/ubuntu/env/bin
-SCRIPT_LOCATION=/home/ubuntu/projets_groupe/Projet_federateur_IA
+ENV_LOCATION=$(pwd)/env/bin
+SCRIPT_LOCATION=$(pwd)/projets_groupe/Projet_federateur_IA
 
 #activation environnement virtuel
 source $ENV_LOCATION/activate
